faf1475f5fe4af1025376f990925d1adf41cc327
yannic <Yannic.wlns@gmail.com>
2019-04-28 17:10:57 +0200

    changes

--- external/include/Utils/Camera.h
+++ external/include/Utils/Camera.h
@@ -7,6 +7,9 @@
 
 #include <vector>
 
+#include <string>
+#include <iostream>
+
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum Camera_Movement {
 	FORWARD,
@@ -21,6 +24,7 @@ const float PITCH = 0.0f;
 const float SPEED = 2.5f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 45.0f;
+int line = 3;
 
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
@@ -69,15 +73,27 @@ public:
 	// Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
 	void ProcessKeyboard(Camera_Movement direction, float deltaTime)
 	{
-		float velocity = MovementSpeed * deltaTime;
-		if (direction == FORWARD)
+		//float velocity = MovementSpeed * deltaTime;
+		float velocity = 2;
+		if (direction == FORWARD) {
 			Position += Front * velocity;
-		if (direction == BACKWARD)
-			Position -= Front * velocity;
-		if (direction == LEFT)
-			Position -= Right * velocity;
-		if (direction == RIGHT)
-			Position += Right * velocity;
+		}
+		//if (direction == BACKWARD)
+		//	Position -= Front * velocity;
+		if (direction == LEFT) {
+			if (line > 1){
+				std::cout << line;
+				line -= 1;
+				Position -= Right * velocity;
+			}
+		}
+		if (direction == RIGHT) {
+			if (line < 5) {
+				std::cout << line;
+				line += 1;
+				Position += Right * velocity;
+			}
+		}
 	}
 
 	// Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
